c07b16ae69f6edf0016bebf5c99f7f995858211c
77CaptainClaw77 <kumar1235813@gmail.com>
2019-07-06 09:33:32 +0530

    All Ready

--- firstone/settings.py
+++ firstone/settings.py
@@ -120,12 +120,12 @@ WSGI_APPLICATION = 'firstone.wsgi.application'
 #         'PORT': '5432'
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config()
